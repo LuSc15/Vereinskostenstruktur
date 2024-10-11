f807bfaa1797c01052198384198dd24eaae14aee
LuSc15 <luca.schmieding@gmail.com>
2024-10-11 06:50:44 +0200

    Diverse Funktionen in der Klasse Verein ergänzt

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             AmateurSportler A1 = new AmateurSportler("Rolf", 10);
+            AmateurSportler A2 = new AmateurSportler("Frank", 10);
+            AmateurSportler A3 = new AmateurSportler("Merlin", 10);
+            Verein V1 = new Verein("Kleingarten eV", 2);
+            V1.MitgliedHinzufuegen(A1);
+            V1.MitgliedHinzufuegen(A2);
+            V1.MitgliedHinzufuegen(A3);
+            V1.Ausgabe();
+
             Console.WriteLine($"Einnahmen:{A1.GetEinnahmen()}");
             Console.WriteLine($"Ausgaben:{A1.GetAusgaben()}");
 
--- Verein.cs
+++ Verein.cs
@@ -9,7 +9,7 @@ namespace Vereinskostenstruktur
     {
 
 
-        private List<Mitglied> _Mitgliederliste;
+        private List<Mitglied> _Mitgliederliste = new List<Mitglied>();
         private int _maxMitglieder;
 
         public Verein(string name, int max) : base(name)
@@ -35,9 +35,33 @@ namespace Vereinskostenstruktur
             }
             return gesamtEinnahmen;
         }
+        public override double GetUeberschuss()
+        {
+            return GetEinnahmen() - GetAusgaben();
+        }
         public override void Ausgabe()
         {
-            throw new NotImplementedException();
+            
+            Console.WriteLine($"Vereinsname: {_name}");
+            Console.WriteLine($"{"Name",-10}{"Einnahmen",10},{"Ausgaben",10}{"Überschuss",10}");
+            foreach (Mitglied m in _Mitgliederliste)
+            {
+                Console.WriteLine($"{m.getName(),-12}{m.GetEinnahmen(),-10}{m.GetAusgaben(),-10}{m.GetUeberschuss(),-10}");
+            }
+            Console.WriteLine($"{"Gesamt:",-12}{this.GetEinnahmen(),-10}{this.GetAusgaben(),-10}{this.GetUeberschuss(),-10}");
+        }
+        public void MitgliedHinzufuegen(Mitglied mitglied)
+        { 
+            if(_Mitgliederliste.Count < _maxMitglieder)
+            {
+                _Mitgliederliste.Add(mitglied);
+                Console.WriteLine($"{mitglied.getName()} ist dem Verein {this._name} beigetreten.");
+            }
+            else
+            {
+                Console.WriteLine($"{mitglied.getName()} konnte nicht beitreten: {this._name} ist voll.");
+            }
+        
         }
     }
 }
